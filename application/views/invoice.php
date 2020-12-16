6590897152f7f026f1f669b5ff000f5e366d0ce0
unknown <information@ebuildix.com>
2020-12-16 10:41:33 +0100

    initialization

---
+++ application/views/invoice.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DREAM
+ * Date: 12/13/2020
+ * Time: 10:10 PM
+ */
+?>
+
+<div class="mainBg">
+  <div class="kt-container col-md-8" >
+
+    <div class = "row mt-5 mb-5 ml-1">
+
+      <h3 class="dayHead" >
+        <i class="flaticon-file-2"></i> &nbspMY INVOICES
+
+      </h3>
+
+    </div>
+    <div class = "kt-datatable" id = "invoiceTable"></div>
+
+    <div class = "row mb-2 kt-align-right mt-5 mb-5">
+      <div class = "col-md-12 mt-5 mb-5">
+        <button class = "btn btn-md btn-save ml-5 kt-align-right ">Download <i class="flaticon2-download-2"></i></button>
+      </div>
+    </div>
+  </div>
+</div>
+<script src = "<?=base_url()?>assets/js/index/account/invoice.js" type = "text/javascript"></script>
+
---
+++ invoice.php
@@ -0,0 +1,44 @@
+{
+"meta": {
+"page": 1,
+"pages": 35,
+"perpage": 10,
+"total": 350,
+"sort": "asc",
+"field": "ShipDate"
+},
+"data": [
+{
+"invoice": "USSI206055",
+"amount": "€9.99",
+"issued": "Jan,31,2020",
+"due": "Jan,31,2020",
+"status": "paid"
+},{
+"invoice": "USSI206055",
+"amount": "€9.99",
+"issued": "Jan,31,2020",
+"due": "Jan,31,2020",
+"status": "paid"
+},
+{
+"invoice": "USSI206055",
+"amount": "€9.99",
+"issued": "Jan,31,2020",
+"due": "Jan,31,2020",
+"status": "paid"
+},
+{
+"invoice": "USSI206055",
+"amount": "€9.99",
+"issued": "Jan,31,2020",
+"due": "Jan,31,2020",
+"status": "paid"
+}
+
+
+
+
+
+]
+}
